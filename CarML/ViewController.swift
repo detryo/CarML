72058ae946b6184ec010971530d2baeaa269ceb7
Cristian Sedano Arenas <cristiansedanoarenas@MacBook-Pro-de-Cristian.local>
2018-09-12 17:21:13 +0100

    UI Done
    Implement new ViewController, first ViewController, implement Image View, Label and Button, second ViewController, implement Vertical Stack View, four Label, two Segmented Control and Slider

--- CarML/ViewController.swift
+++ CarML/ViewController.swift
@@ -9,17 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    override var prefersStatusBarHidden: Bool {
+        return true // true para ocultar y false para no ocultarla
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
